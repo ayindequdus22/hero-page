e3364e767cabab72a6ef86d5fd701f6b534e926d
ayindequdus22 <abdulqudusayinde60@gmail.com>
2022-07-06 12:22:09 -0700

    my first commit

---
+++ ecommerce.js
@@ -0,0 +1,18 @@
+'use strict';
+alert('Welcome To Anteqs Clothing')
+document.querySelector('.search-for').classList.add('d-none');
+
+$(document).ready(function(){
+$('.search-icon').click(function(){
+document.querySelector('.search-for').classList.toggle('d-none');
+});
+
+$('.navbar-toggler-icon').click(function(){
+    document.querySelector('.search-container').classList.toggle('d-none');
+});
+ 
+$('#search_box').click(function(){
+    $('.search').hide();
+});
+
+})
